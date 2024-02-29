157ea38fc9c01ad97e04e2e120fb3d0fa7963c2a
Xolisakesi <xolisakesi@gmail.com>
2024-02-29 14:29:36 +0200

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,9 +15,14 @@ binary_tree_t *new_node;
 if (parent == NULL)
 return (NULL);
 
-new_node = binary_tree_node(parent, value);
+new_node = malloc(sizeof(binary_tree_t));
 if (new_node == NULL)
 return (NULL);
+
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
 
 if (parent->left != NULL)
 {
@@ -27,5 +32,26 @@ parent->left->parent = new_node;
 parent->left = new_node;
 
 return (new_node);
+}
+/**
+* binary_tree_nodes - Counts the nodes with at least one child in a binary tree
+* @tree: Pointer to the root node of the tree to count the number of nodes
+*
+* Return: Number of nodes with at least one child in the binary tree
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+size_t nodes = 0;
+
+if (tree == NULL)
+return (0);
+
+if (tree->left != NULL || tree->right != NULL)
+nodes++;
+
+nodes += binary_tree_nodes(tree->left);
+nodes += binary_tree_nodes(tree->right);
+
+return (nodes);
 }
 
