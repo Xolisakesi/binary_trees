faca36d0010875d99dd122ec624cc849e41ff4ca
Xolisakesi <xolisakesi@gmail.com>
2024-03-01 01:52:14 +0200

    112-array_to_bst.c

---
+++ 112-array_to_bst.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+#include "binary_trees.h"
+
+/**
+* binary_tree_delete - Deletes an entire binary tree
+* @tree: Pointer to the root node of the tree to delete
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+if (tree == NULL)
+return;
+
+binary_tree_delete(tree->left);
+binary_tree_delete(tree->right);
+free(tree);
+}
+
+/**
+* array_to_bst - Builds a Binary Search Tree (BST) from an array.
+* @array: A pointer to the first element of the array to be converted.
+* @size: The number of elements in the array.
+*
+* Return: A pointer to the root node of the created BST, or NULL on failure.
+*/
+bst_t *array_to_bst(int *array, size_t size)
+{
+bst_t *root = NULL;
+size_t i;
+
+if (!array || size == 0)
+return (NULL);
+
+for (i = 0; i < size; i++)
+{
+if (!bst_insert(&root, array[i]))
+{
+/* If insertion fails, destroy the tree and return NULL */
+binary_tree_delete(root);
+return (NULL);
+}
+}
+
+return (root);
+}
+
